c03fa6c4e7d944f8d1e53d392b7f09e5051aa2f2
Giladinator <gilad.handler@gmail.com>
2017-04-06 10:36:04 +0100

    Initial Commit

---
+++ DistroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DistroyTheWorld
+//
+//  Created by Gilad Handler on 06/04/2017.
+//  Copyright © 2017 Gilad Handler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
